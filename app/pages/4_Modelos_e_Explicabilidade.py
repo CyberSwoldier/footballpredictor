ec8516c8c8f34f249c55f286602d4a2ed391a90e
CyberSwoldier <mendespinto93@gmail.com>
2026-02-05 14:17:20 +0000

    Create 4_Modelos_e_Explicabilidade.py

---
+++ app/pages/4_Modelos_e_Explicabilidade.py
@@ -0,0 +1,13 @@
+import streamlit as st
+
+st.title("🧠 Modelos e Explicabilidade")
+
+st.markdown("""
+Aqui podes documentar:
+
+- Que modelos foram usados (Poisson, RandomForest, etc.)
+- Que features entram nos modelos
+- Como são avaliados
+- Como interpretar previsões
+- Futuras melhorias (xG, xGOT, redes neurais, etc.)
+""")
